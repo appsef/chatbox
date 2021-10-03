9fa1f3736a7609f9ab0b9217d41770146ee9be8e
Alok J Kashyap <alokjkashyap@gmail.com>
2021-10-03 17:26:27 +0530

    New Add Post section
    Add Post Button and Section in Post section

--- src/components/PostSection.js
+++ src/components/PostSection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Post from './Post'
-import users from './users.json'
 import posts from './posts.json'
+import AddPost from './AddPost'
 
 
 const PostSection = () => {
@@ -9,6 +9,7 @@ const PostSection = () => {
 
     return (
         <div className="col-span-4 md:col-span-3  flex flex-col pt-20">
+        <AddPost />
         {posts.map(post => ( <Post post={post}/> ))}
         </div>
     )
